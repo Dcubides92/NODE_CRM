14d68783fc84d3e0bc87ea3d168d84ffa38a40cd
Dcubides92 <92190294+Dcubides92@users.noreply.github.com>
2021-12-14 22:26:37 -0500

    Actualización para modificar y filtro de clientes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ var express = require("express");
 var app = express();
 var path = require("path");
 var bodyParser = require("body-parser");
+var alert = require("alert");
 var mongoose = require("mongoose");
-var textSearch = require("mongoose-text-search");
+//var textSearch = require("mongoose-text-search"); */
 
 mongoose.connect("mongodb+srv://DIEGO92:Javier17@cluster0.ygwwk.mongodb.net/CRM_Bictia?retryWrites=true&w=majority")
     .then(function (db) {
@@ -28,14 +29,12 @@ app.use(express.static(__dirname + '/public/css'));
 //inicio
 app.get('/', async function (req, res) {
     var t = await Clientes.find();
-    //console.log(req)
     res.render('index', {
         clientes: t
     });
 });
 app.get('/inicio', async function (req, res) {
     var t = await Clientes.find();
-    //console.log(req)
     res.render('index', {
         clientes: t
     });
@@ -58,9 +57,14 @@ app.get('/historial', async function (req, res) {
 
 // para agregar un nuevo cliente
 app.post('/nuevoCliente', async function (req, res) {
-    var t = new Clientes(req.body);
-    await t.save();
-    res.redirect('/inicio');
+    if (req.body.estado==="Seleccionar una Opción" || req.body.t_documento==="Seleccionar una Opción"){
+        alert("no ha seleccionado un Estado o un Tipo de documento");
+    }
+    else{
+        var t = new Clientes(req.body);
+        await t.save();
+        res.redirect('/inicio');
+    }
 })
 
 //Modificar Informacion Genetal 
@@ -79,7 +83,6 @@ app.get('/modificar/:id', async function (req, res) {
 app.post('/modificarClientes', async function (req, res) {
     var body = req.body;
     var id = req.body.id;
-    console.log(body)
     await Clientes.updateOne({ _id: id }, { $set: body })
         .catch(function (err) {
             console.log(err);
@@ -109,44 +112,60 @@ app.get('/estado/:id', async function (req, res) {
 // Modificar el estado del cliente
 app.post('/modificarestado', async function (req, res) {
     //realiza el cambio en el cliente
-    var id = req.body.id_cliente;
-    var nom = req.body.cliente;
-    var estate = req.body.estado;
-    await Clientes.updateOne({ _id: id }, { estado: estate })
-        .catch(function (err) {
-            console.log(err);
-        })
+    console.log(req.body.estado);
+    console.log(req.body.estado_actual);
+    if (req.body.estado===req.body.estado_actual){
+        alert("no ha seleccionado un Estado diferente al actual");
+    }
+    else{
+        var id = req.body.id_cliente;
+        var nom = req.body.cliente;
+        var estate = req.body.estado;
+        await Clientes.updateOne({ _id: id }, { estado: estate })
+            .catch(function (err) {
+                console.log(err);
+            })
 
-    //ingresa el historial de cambios
-    var t = new Estado(req.body);
-    await t.save();
+        //ingresa el historial de cambios
+        var t = new Estado(req.body);
+        await t.save();
 
-    var t = await Estado.find({ id_cliente: id });
-    res.render('estado', {
-        estado: t,
-        id: id,
-        cliente: nom,
-        estado_actual: estate
-    });
-
+        var t = await Estado.find({ id_cliente: id });
+        res.render('estado', {
+            estado: t,
+            id: id,
+            cliente: nom,
+            estado_actual: estate
+        });
+    }
 })
 
 //filtrar el formulario
 app.post('/filtro', async function (req, res) {
+    var estate = req.body.estado
     var nom = req.body.nombre;
-    
-    var t = await Clientes.find({
-        nombre: { $regex: nom, $options: 'i' }
-    });
-
-    res.render('index', {
-        clientes: t,
-    });
-
+    if (estate==="Selecionar una Opción" && nom===""){
+        var t = await Clientes.find();
+        res.render('index', {clientes: t});
+    }
+    if (estate!="Selecionar una Opción" && nom===""){
+        var t = await Clientes.find({estado: estate});
+        res.render('index', {clientes: t});
+    }
+    if (estate==="Selecionar una Opción" && nom!=""){
+        var t = await Clientes.find({nombre: { $regex: nom, $options: 'i' }});
+        res.render('index', {clientes: t});
+    }
+    if (estate!="Selecionar una Opción" && nom!=""){
+        var t = await Clientes.find({
+            nombre: { $regex: nom, $options: 'i' },
+            estado: estate
+        });
+        res.render('index', {clientes: t});
+    }    
 })
 
-
-//Se Elimina los Docuemntos 
+//Se Elimina los Documentos 
 
 app.get('/eliminar/:id/', async function (req, res) {
     var id = req.params.id;
@@ -162,3 +181,15 @@ app.listen(3000, function () {
     console.log("Se esta conectando por el puerto 3000 al Servidor");
 });
 
+//funcion solo numero
+
+/*  function format(input)
+{
+    var nume = input.value.replace(/\,/g,'');
+    if(!isNaN(nume)){
+        input.value = nume;
+    }
+    else{ alert('Solo se permiten numeros');
+        input.value = input.value.replace(/[^\d\.]* /g,'');
+    }
+}   */
