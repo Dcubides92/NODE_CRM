75eeae0471d85106ab3afaf3101a87e1b298f2e0
Cesar Vargas <cavargas769@misena.edu.co>
2021-12-16 17:59:20 -0500

    se guarda cambios ok se deshabilita en modificar No. documento y Tipo documento

--- NODE_CRM/src/app.js
+++ NODE_CRM/src/app.js
@@ -7,13 +7,13 @@ var mongoose = require("mongoose");
 //var textSearch = require("mongoose-text-search"); */
 
 mongoose.connect("mongodb+srv://DIEGO92:Javier17@cluster0.ygwwk.mongodb.net/CRM_Bictia?retryWrites=true&w=majority")
-    .then(function (db) {
+    .then(function(db) {
         console.log("Conectado a al Base de Datos");
     })
-    .catch(function (err) {
+    .catch(function(err) {
         console.log(err);
     })
-//configuraciones
+    //configuraciones
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,13 +27,13 @@ app.use(express.static(__dirname + '/public/css'));
 
 
 //inicio
-app.get('/', async function (req, res) {
+app.get('/', async function(req, res) {
     var t = await Clientes.find();
     res.render('index', {
         clientes: t
     });
 });
-app.get('/inicio', async function (req, res) {
+app.get('/inicio', async function(req, res) {
     var t = await Clientes.find();
     res.render('index', {
         clientes: t
@@ -41,13 +41,13 @@ app.get('/inicio', async function (req, res) {
 });
 
 //formulario para nuevos clientes
-app.get('/cliente', async function (req, res) {
+app.get('/cliente', async function(req, res) {
     //console.log(req)
     res.render('cliente');
 });
 
 //historial de cambios en clientes
-app.get('/historial', async function (req, res) {
+app.get('/historial', async function(req, res) {
     var t = await Estado.find();
     //console.log(req)
     res.render('historial', {
@@ -56,11 +56,10 @@ app.get('/historial', async function (req, res) {
 });
 
 // para agregar un nuevo cliente
-app.post('/nuevoCliente', async function (req, res) {
-    if (req.body.estado==="Seleccionar una Opción" || req.body.t_documento==="Seleccionar una Opción"){
+app.post('/nuevoCliente', async function(req, res) {
+    if (req.body.estado === "Seleccionar una Opción" || req.body.t_documento === "Seleccionar una Opción") {
         alert("no ha seleccionado un Estado o un Tipo de documento");
-    }
-    else{
+    } else {
         var t = new Clientes(req.body);
         await t.save();
         res.redirect('/inicio');
@@ -68,7 +67,7 @@ app.post('/nuevoCliente', async function (req, res) {
 })
 
 //Modificar Informacion Genetal 
-app.get('/modificar/:id', async function (req, res) {
+app.get('/modificar/:id', async function(req, res) {
     console.log(req)
     var id = req.params.id;
     var t = await Clientes.findOne({ _id: id });
@@ -80,20 +79,20 @@ app.get('/modificar/:id', async function (req, res) {
 })
 
 //Para cambiar los datos del cliente
-app.post('/modificarClientes', async function (req, res) {
+app.post('/modificarClientes', async function(req, res) {
     var body = req.body;
     var id = req.body.id;
     await Clientes.updateOne({ _id: id }, { $set: body })
-        .catch(function (err) {
+        .catch(function(err) {
             console.log(err);
         })
-    //var t = await Clientes.find();    
+        //var t = await Clientes.find();    
     res.redirect('/inicio');
 })
 
 
 //mostrar el historial e ir al formulario para modificar el estado
-app.get('/estado/:id', async function (req, res) {
+app.get('/estado/:id', async function(req, res) {
     var id = req.params.id;
     var t = await Clientes.findOne({ _id: id });
     var nom = t.nombre;
@@ -110,19 +109,18 @@ app.get('/estado/:id', async function (req, res) {
 })
 
 // Modificar el estado del cliente
-app.post('/modificarestado', async function (req, res) {
+app.post('/modificarestado', async function(req, res) {
     //realiza el cambio en el cliente
     console.log(req.body.estado);
     console.log(req.body.estado_actual);
-    if (req.body.estado===req.body.estado_actual){
+    if (req.body.estado === req.body.estado_actual) {
         alert("no ha seleccionado un Estado diferente al actual");
-    }
-    else{
+    } else {
         var id = req.body.id_cliente;
         var nom = req.body.cliente;
         var estate = req.body.estado;
         await Clientes.updateOne({ _id: id }, { estado: estate })
-            .catch(function (err) {
+            .catch(function(err) {
                 console.log(err);
             })
 
@@ -141,33 +139,33 @@ app.post('/modificarestado', async function (req, res) {
 })
 
 //filtrar el formulario
-app.post('/filtro', async function (req, res) {
+app.post('/filtro', async function(req, res) {
     var estate = req.body.estado
     var nom = req.body.nombre;
-    if (estate==="Selecionar una Opción" && nom===""){
+    if (estate === "Selecionar una Opción" && nom === "") {
         var t = await Clientes.find();
-        res.render('index', {clientes: t});
+        res.render('index', { clientes: t });
     }
-    if (estate!="Selecionar una Opción" && nom===""){
-        var t = await Clientes.find({estado: estate});
-        res.render('index', {clientes: t});
+    if (estate != "Selecionar una Opción" && nom === "") {
+        var t = await Clientes.find({ estado: estate });
+        res.render('index', { clientes: t });
     }
-    if (estate==="Selecionar una Opción" && nom!=""){
-        var t = await Clientes.find({nombre: { $regex: nom, $options: 'i' }});
-        res.render('index', {clientes: t});
+    if (estate === "Selecionar una Opción" && nom != "") {
+        var t = await Clientes.find({ nombre: { $regex: nom, $options: 'i' } });
+        res.render('index', { clientes: t });
     }
-    if (estate!="Selecionar una Opción" && nom!=""){
+    if (estate != "Selecionar una Opción" && nom != "") {
         var t = await Clientes.find({
             nombre: { $regex: nom, $options: 'i' },
             estado: estate
         });
-        res.render('index', {clientes: t});
-    }    
+        res.render('index', { clientes: t });
+    }
 })
 
 //Se Elimina los Documentos 
 
-app.get('/eliminar/:id/', async function (req, res) {
+app.get('/eliminar/:id/', async function(req, res) {
     var id = req.params.id;
     console.log("Se esta eliminando " + id);
     var clientes = await Clientes.findById(id);
@@ -177,7 +175,7 @@ app.get('/eliminar/:id/', async function (req, res) {
 
 //Servidor 
 
-app.listen(3000, function () {
+app.listen(3000, function() {
     console.log("Se esta conectando por el puerto 3000 al Servidor");
 });
 
@@ -193,3 +191,5 @@ app.listen(3000, function () {
         input.value = input.value.replace(/[^\d\.]* /g,'');
     }
 }   */
+
+// pais ciudad
